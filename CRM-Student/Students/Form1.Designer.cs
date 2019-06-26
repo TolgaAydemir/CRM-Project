56c2213dbe4a9eb8d3d7f68edf4710ad0bebae88
tolgaaydemir <aydemir.tolga895@gmail.com>
2019-06-26 19:04:01 +0300

    Fake Data kullanılarak öğrenci yönetim sistemi oluşturuldu

---
+++ CRM-Student/Students/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Students
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.averagelabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(654, 476);
+            this.newButton.Margin = new System.Windows.Forms.Padding(2);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(84, 40);
+            this.newButton.TabIndex = 0;
+            this.newButton.Text = "New Student";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 57);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(542, 462);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
+            // 
+            // textSearch
+            // 
+            this.textSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textSearch.Location = new System.Drawing.Point(77, 24);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(260, 22);
+            this.textSearch.TabIndex = 2;
+            this.textSearch.TextChanged += new System.EventHandler(this.textSearch_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(22, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Search";
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Location = new System.Drawing.Point(419, 24);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(121, 21);
+            this.stateComboBox.TabIndex = 4;
+            this.stateComboBox.SelectedIndexChanged += new System.EventHandler(this.stateComboBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(356, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "State";
+            // 
+            // averagelabel
+            // 
+            this.averagelabel.AutoSize = true;
+            this.averagelabel.Location = new System.Drawing.Point(660, 433);
+            this.averagelabel.Name = "averagelabel";
+            this.averagelabel.Size = new System.Drawing.Size(35, 13);
+            this.averagelabel.TabIndex = 7;
+            this.averagelabel.Tag = "averagelabel";
+            this.averagelabel.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 524);
+            this.Controls.Add(this.averagelabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stateComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox stateComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label averagelabel;
+    }
+}
+
---
+++ CRM-Student/Students/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Students
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        int sortedColumn = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            var form2 = new Form2(this);
+            form2.Show();
+        }
+
+        private void generateStudents()
+        {
+            var student = new Student();
+            for (int i = 1; i <= 100; i++)
+            {
+                students.Add(student.generateStudent(i));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(800, 600);
+
+            generateStudents();
+            dataGridView1.DataSource = students;
+            dataGridView1.Columns[0].HeaderText = "Student ID";
+            dataGridView1.Columns[1].HeaderText = "Name";
+            dataGridView1.Columns[2].HeaderText = "Surname";
+            dataGridView1.Columns[3].HeaderText = "Phone";
+            dataGridView1.Columns[4].HeaderText = "Email";
+            dataGridView1.Columns[5].HeaderText = "State";
+            dataGridView1.Columns[6].HeaderText = "Grade";
+
+
+
+            fillStateComboBox();
+
+            averagelabel.Text = "Average : " + students.Average(x => x.grade);
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var studentId = (int)dataGridView1.CurrentRow.Cells[0].Value;
+                var student = (Student)students.Find(x => x.studentId == studentId);
+                var form2 = new Form2(this, student);
+                form2.Show();
+            }
+        }
+
+        public void insertStudent(Student student)
+        {
+            student.studentId = students.Count + 1;
+            students.Add(student);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = students;
+        }
+        public void deleteStudent(Student student)
+        {
+            students.Remove(student);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = students;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+        }
+
+        private void textSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (textSearch.Text.Length == 0)
+            {
+                dataGridView1.DataSource = students;
+            }
+            else
+            {
+                var searchStudents = students.FindAll(x => x.name.ToLower().Contains(textSearch.Text) || x.surname.ToLower().Contains(textSearch.Text));
+
+                dataGridView1.DataSource = searchStudents;
+            }
+        }
+        private void fillStateComboBox()
+        {
+
+            var state = students.GroupBy(x => x.state).Select(x => x.First())
+                     .OrderBy(x => x.state).ToList();
+            var s = new Student();
+            state.Insert(0, s);
+            stateComboBox.DataSource = state;
+            stateComboBox.DisplayMember = "State";
+            stateComboBox.ValueMember = "State";
+
+
+
+
+        }
+
+        private void stateComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (stateComboBox.SelectedIndex == 0)
+            {
+                dataGridView1.DataSource = students;
+            }
+            else
+            {
+                var seacrhedStudents = students.FindAll(x => x.state == stateComboBox.SelectedValue.ToString());
+                dataGridView1.DataSource = seacrhedStudents;
+            }
+
+        }
+
+        private void averagebutton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            switch (e.ColumnIndex)
+            {
+                case 0:
+                    if (sortedColumn == e.ColumnIndex)
+                    {
+                        dataGridView1.DataSource = students.OrderByDescending(x => x.studentId).ToList();
+                        sortedColumn = -1;
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = students.OrderBy(x => x.studentId).ToList();
+                        sortedColumn = e.ColumnIndex;
+                    }
+                    
+                    break;
+                
+                case 1:
+                    if (sortedColumn == e.ColumnIndex)
+                    {
+                        dataGridView1.DataSource = students.OrderByDescending(x => x.name).ToList();
+                        sortedColumn = -1;
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = students.OrderBy(x => x.name).ToList();
+                        sortedColumn = e.ColumnIndex;
+                    }
+
+                    break;
+                case 2:
+                    if (sortedColumn == e.ColumnIndex)
+                    {
+                        dataGridView1.DataSource = students.OrderByDescending(x => x.surname).ToList();
+                        sortedColumn = -1;
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = students.OrderBy(x => x.surname).ToList();
+                        sortedColumn = e.ColumnIndex;
+                    }
+
+                    break;
+                case 3:
+                    if (sortedColumn == e.ColumnIndex)
+                    {
+                        dataGridView1.DataSource = students.OrderByDescending(x => x.phone).ToList();
+                        sortedColumn = -1;
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = students.OrderBy(x => x.phone).ToList();
+                        sortedColumn = e.ColumnIndex;
+                    }
+
+                    break;
+                case 4:
+                    if (sortedColumn == e.ColumnIndex)
+                    {
+                        dataGridView1.DataSource = students.OrderByDescending(x => x.email).ToList();
+                        sortedColumn = -1;
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = students.OrderBy(x => x.email).ToList();
+                        sortedColumn = e.ColumnIndex;
+                    }
+
+                    break;
+                case 5:
+                    if (sortedColumn == e.ColumnIndex)
+                    {
+                        dataGridView1.DataSource = students.OrderByDescending(x => x.state).ToList();
+                        sortedColumn = -1;
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = students.OrderBy(x => x.state).ToList();
+                        sortedColumn = e.ColumnIndex;
+                    }
+
+                    break;
+                case 6:
+                    if (sortedColumn == e.ColumnIndex)
+                    {
+                        dataGridView1.DataSource = students.OrderByDescending(x => x.grade).ToList();
+                        sortedColumn = -1;
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = students.OrderBy(x => x.grade).ToList();
+                        sortedColumn = e.ColumnIndex;
+                    }
+
+                    break;
+                default:
+                    break;
+
+
+            }
+        }
+    }
+}
